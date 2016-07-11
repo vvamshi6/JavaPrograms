1847fa4460846bb0ff135b86c7c62df8b860a3f4
vvamshi6 <vvamshi6@gmail.com>
2016-07-11 19:12:03 +0530

    Hello UserName program

---
+++ Hello.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+/*
+* created by: Bridge Labz
+* Date 01/07/2016
+*
+* Purpose:Taking User Input and Replace String Template “Hello <<UserName>>, How are you?” 
+**/
+import com.bridgelabz.Utility;
+public class Hello{
+	public static void main(String[] args){
+		Utility u = new Utility();
+		Hello h = new Hello();
+		//taking input from user
+		System.out.println("Enter user Name");
+		String name = u.inputString();
+		//calling user name method
+		h.userName(name);
+  }
+		//displaying prompt
+	public void userName(String name){		
+		System.out.println("Hello "+name+", How are you?");
+	}
+
+}
